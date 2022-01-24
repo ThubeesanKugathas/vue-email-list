03471a1ad0c6efa0ddf98cf176bb7d2053a3f72b
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-24 14:36:10 +0100

    list of emails printed by axios html/js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#app",
+    data: {
+        emailList: [
+
+        ]
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+                this.emailList.push(email.data.response);
+            })
+        }
+    }
+
+})
